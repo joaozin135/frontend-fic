08e867e5375491bdbf57af5a304c77cf8bd42df4
AmandaSouza5 <junior.joao1305@gmail.com>
2026-07-09 21:37:45 -0300

    feat: criação página cursos

---
+++ src/pages/CoursesPage/index.tsx
@@ -0,0 +1,10 @@
+export function CoursesPage(){
+    return(
+        <section className="space-y-6">
+            <div>
+                <h3 className="text-2xl font-bold">Resumo</h3>
+                <p className="text-slate-600">Visão geral do painel administrativo.</p>
+            </div> 
+        </section>
+    )
+}
