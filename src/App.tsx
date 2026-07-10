e099e48722a3085d2433dec66a242f19cbaf6662
AmandaSouza5 <junior.joao1305@gmail.com>
2026-07-09 21:36:51 -0300

    feat: add rotas novas páginas

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import { DashboardLayout } from './components/layoult/DashboardLayoult'
 import { DashboardHome } from './pages/DashboardHome'
 import { UsersPage } from './pages/UsersPage'
+import { CategoryPage } from './pages/CategoryPage'
+import { CoursesPage } from './pages/CoursesPage'
 
 function App() {
   return (
     <Routes>
       <Route element={<DashboardLayout />}>
         <Route index element={<DashboardHome />} />
-        <Route path="produtos" element={<UsersPage />} />
+        <Route path="usuarios" element={<UsersPage />} />
+        <Route path="categorias" element={<CategoryPage />} />
+        <Route path="cursos" element={<CoursesPage />} />
       </Route>
     </Routes>
   )
